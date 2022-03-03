01612de1f8ea63013683de8b2f6a1c40c7bb383e
Ignacio Ainol <iggnaxios@gmail.com>
2022-03-02 23:23:10 -0300

    testing send email f*cking ses aws

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const fileUpload = require('express-fileupload');
+const aws = require('aws-sdk');
+
+const ses = new aws.SES({
+    region: 'sa-east-1',
+    secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+});
 
 //config
 app.set('port', 3000 || process.env.PORT);
@@ -21,8 +28,35 @@ app.use(fileUpload({
 app.use(require('./routes'));
 
 app.post('/email', (req, res) => {
-    res.send("ok route email");
-})
+    const { email, message, name } = req.body;
+    sesTest('ignacio.ainolrivera@gmail.com', email, message, name).then((val) => {
+        console.log('go this back', val);
+        res.send("successful");
+    }).catch((err) => {
+        res.send(err);
+    })
+});
+
+function sesTest(emailTo, emailFrom, message, name) {
+    const params = {
+        Destination: {
+            ToAddresses: [emailTo]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: "From contact xxxx" + name + "\n" + message
+                }
+            },
+            Subject: {
+                Data: "Name: " + emailFrom
+            }
+        },
+        Source: "iggnaxios@gmail.com"
+    };
+
+    return ses.sendEmail(params).promise()
+}
 
 
 // Echamos a correr la api en el puerto 3000 o el que esta definido en las variables de entorno (archivo .env)
